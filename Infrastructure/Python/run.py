eb4b160e29dbedb072e03f6d73a332e8a1bff1ff
Zachary <zachary.chester@biola.edu>
2019-02-26 20:04:45 -0800

    Updated api

--- Infrastructure/Python/run.py
+++ Infrastructure/Python/run.py
@@ -4,7 +4,7 @@ from json import dumps, loads
 app = Flask(__name__)
 
 #--/api/user--
-@app.route('/api/user', methods=['GET', 'POST', 'DELETE'])
+@app.route('/api/user', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def user():
     resp = {}
     status = 404
@@ -44,12 +44,37 @@ def user():
             last_name = json_data.get("last_name")
             email = json_data.get("email")
             password = json_data.get("password")
-            #SQL INPUT first_name, last_name, email, password
-            resp = {"request_type":"POST","message":f"User {first_name} {last_name} Successfully Created"}
+            #SQL INSERT first_name, last_name, email, password
+            user_id = "3"
+            resp = {"request_type":"POST","message":f"User {user_id} Successfully Created"}
+            status = 200
+
+    #--PUT--
+    # @json: {user_id, first_name, last_name, email, password} , returns: {first_name, last_name, email, password}
+    elif request.method == 'PUT':
+        json_data = request.get_json(force=True)
+        if not json_data:
+            resp = {"err": "No data provided"}
+            status = 400
+        elif ("user_id") not in json_data:
+            resp = {"err": "Missing required fields"}
+            status = 400
+        else:
+            user_id = json_data.get("user_id")
+            if "first_name" in json_data:
+                first_name = json_data.get("first_name")
+            if "last_name" in json_data:
+                last_name = json_data.get("last_name")
+            if "email" in json_data:
+                email = json_data.get("email")
+            if "password" in json_data:
+                password = json_data.get("password")
+            #SQL UPDATE first_name, last_name, email, password on user_id
+            resp = {"request_type":"PUT","message":f"User {user_id} Successfully Updated"}
             status = 200
 
     #--DELETE--
-    # @args: user_id
+    # @json: user_id
     elif request.method == 'DELETE':
         json_data = request.get_json(force=True)
         if not json_data:
@@ -58,16 +83,17 @@ def user():
         elif ("user_id") not in json_data:
             resp = {"err": "Missing required fields"}
             status = 400
-        user_id = json_data.get("user_id")
-        #SQL DELETE on user_id
-        resp = {"request_type":"DELETE","message":f"User {user_id} Successfully Deleted"}
-        status = 200
+        else:
+            user_id = json_data.get("user_id")
+            #SQL DELETE USER on user_id
+            resp = {"request_type":"DELETE","message":f"User {user_id} Successfully Deleted"}
+            status = 200
 
     return Response(dumps(resp),status=status,mimetype='application/json')
 #END OF --/api/user--
 
 #--/api/user/login--
-@app.route('/api/user/login',methods=['GET'])
+@app.route('/api/user/login', methods=['GET'])
 def login():
     resp = {}
     status = 404
@@ -89,7 +115,7 @@ def login():
             #SQL SELECT email, password --> user_id
             user_id = "3"
             if login:
-                resp = {"request_type":"GET", "message":f"User {user_id} Successfully Logged In"}
+                resp = {"request_type":"GET", "message":f"User {user_id} Successfully Logged In", "user_id": f"{user_id}"}
                 status = 200
             else:
                 resp = {"err: Credentials are invalid"}
@@ -97,7 +123,53 @@ def login():
     return Response(dumps(resp),status=status,mimetype='application/json')
 #END OF --/api/user/login--
 
+#--/api/group--
+@app.route('/api/group', methods=['GET', 'POST', 'DELETE'])
+def group():
+    resp = {}
+    status = 404
 
+    #--GET--
+    # @json: group_id , returns {name, description, color}
+    if request.method == 'GET':
+        json_data = request.get_json(force=True)
+        if not json_data:
+            resp = {"err": "No data provided"}
+            status = 400
+        elif ("group_id") not in json_data:
+            resp = {"err": "Missing required fields"}
+            status = 400
+        else:
+            group_id = json_data.get("group_id")
+            #SQL SELECT group_id --> name, description, color
+            name = "Test Group"
+            description = "Four score and twenty years ago..."
+            color = "Blue"
+            resp = {"request_type":"GET", "name": f"{name}", "description": f"{description}", "color": f"{color}"}
+            status = 200
+
+    #--POST--
+    # @json: {user_id, name, description, color} , returns group_id
+    if request.method == 'POST':
+        json_data = request.get_json(force=True)
+        if not json_data:
+            resp = {"err": "No data provided"}
+            status = 400
+        elif ("user_id" or "name" or "description" or "color") not in json_data:
+            resp = {"err": "Missing required fields"}
+            status = 400
+        else:
+            user_id = json_data.get("user_id")
+            name = json_data.get("name")
+            description = json_data.get("description")
+            color = json_data.get("color")
+            #SQL INSERT user_id, name, description, color
+            group_id = "3"
+            resp = {"request_type":"POST", "message": f"Group {group_id} Successfully Created", "group_id": f"{group_id}"}
+            status = 200
+
+    return Response(dumps(resp),status=status,mimetype='application/json')
+#END OF --/api/group--
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
