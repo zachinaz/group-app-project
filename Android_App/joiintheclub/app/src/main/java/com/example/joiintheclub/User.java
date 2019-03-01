d04a1099e925a9f462f1548e456e531b8e657a0b
Wesley Shiozaki <wesley.p.shiozaki@biola.edu>
2019-02-28 19:59:56 -0800

    Updated/added details to User class

--- Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/User.java
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/User.java
@@ -2,4 +2,39 @@ package com.example.joiintheclub;
 
 public class User {
 
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String pwd;
+
+    //private String profilePicURL;
+    //private int userID;
+    //private boolean isActive;
+
+
+    public boolean createUser(String firstName,
+        String lastName,
+        String email,
+        String pwd)
+    {
+        return true;
+    }
+    private void get() {
+
+    }
+    private void set() {
+
+    }
+
+    public boolean login() {
+        //fix later
+        return false;
+    }
+    public boolean logout() {
+        //fix later
+        return false;
+    }
+
+
 }
