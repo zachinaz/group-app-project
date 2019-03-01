a3133b28805a41e1b57076c28d67964a9b04795c
Hannah Kim <hannahk2405@gmail.com>
2019-02-28 23:15:38 -0800

    hello? this is a test

---
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/Group.java
@@ -0,0 +1,11 @@
+package com.example.joiintheclub;
+import java.util.Date;
+
+public class  group{
+    private String groupName;
+    private int groupID;
+    private int numOfPeople;
+    Date DateCreated;
+    //test
+
+}
