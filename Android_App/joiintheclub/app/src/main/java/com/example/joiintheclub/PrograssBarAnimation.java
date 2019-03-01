70a385859a5f0e8ba1cb95fd409fff7506bea4d2
JIAHUI J <jiahui.jiang@biola.edu>
2019-03-01 15:43:29 -0800

    app icon, loadscreen, and login screen implementation. Login user input verification

---
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/PrograssBarAnimation.java
@@ -0,0 +1,37 @@
+package com.example.joiintheclub;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class PrograssBarAnimation extends Animation {
+
+    private Context context;
+    private ProgressBar prograssBar;
+    TextView textView;
+    private float from;
+    private float to;
+
+    public PrograssBarAnimation(Context context, ProgressBar prograssBar, TextView textView, float from, float to)
+    {
+        this.context = context;
+        this.prograssBar = prograssBar;
+        this.textView = textView;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+        float value = from + (to - from)*interpolatedTime;
+        prograssBar.setProgress((int) value);
+        textView.setText((int)value+" %");
+
+        if(value == to)
+            context.startActivity(new Intent(context, LoginActivity.class));
+    }
+}
