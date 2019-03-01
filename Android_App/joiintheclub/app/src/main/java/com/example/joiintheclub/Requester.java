9253dccce1cfdcc7049cf0d2cbaf47953edd4ea2
Zachary <zachary.chester@biola.edu>
2019-02-28 18:24:42 -0800

    Began implementing Requester class

--- Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/Requester.java
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/Requester.java
@@ -9,25 +9,38 @@ import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.LinkedHashMap;
 
 public class Requester {
 
     private static String baseUrl = "localhost:5000/api";
 
-    public static void main(String targetApiUrl, String method, String[] args) {
+    //Call requester to interface when needing to interface with the api
+    //  @param String targetUrl : the specific api resource that the information is stored on.
+    //      e.g. '/user' when needing to get, create, update, or delete user information
+    //  @param String method : the desired method needed
+    //      e.g. 'GET' when wanting to pull information from the api
+    //  @param LinkedHashMap<String, String> dict : a map of keys (e.g. "first_name") and values (e.g. "Bob") needed for the api request
+    //      e.g. {"first_name", "last_name"} when wanting to update the first name and last name of a user on the api
+    //  returns LinkedHashMap<String, String> dict : a map of keys and values (see above) of information from the api
+    public static LinkedHashMap<String, String> requester(String targetUrl, String method, LinkedHashMap<String, String> dict) {
 
         try {
-            URL object = new URL(baseUrl);
+            //Create URL object from static base url plus the targeted API
+            String totalUrl = baseUrl + targetUrl;
+            URL object = new URL(totalUrl);
 
             HttpURLConnection con = (HttpURLConnection) object.openConnection();
             con.setDoOutput(true);
             con.setDoInput(true);
             con.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
             con.setRequestProperty("Accept", "application/json");
-            con.setRequestMethod(method);
+            con.setRequestMethod(method.toUpperCase());
 
             JSONObject parent = new JSONObject();
-            
+
+
+
             OutputStreamWriter wr = new OutputStreamWriter(con.getOutputStream());
             wr.write(parent.toString());
             wr.flush();
@@ -54,7 +67,7 @@ public class Requester {
         }
 
 
-
+        return dict;
     }
 
 }
