6f649354cc5435c1e5c05862f8bf6369b7ce19d5
Zachary <zachary.chester@biola.edu>
2019-03-01 16:12:45 -0800

    Integrated createUser function partially with Requester.requester

--- Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/User.java
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/User.java
@@ -1,6 +1,9 @@
 package com.example.joiintheclub;
 
-import java.util.LinkedHashMap;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 public class User {
 
@@ -14,18 +17,22 @@ public class User {
     //private String profilePicURL;
     //private int userID;
     private static boolean isActive;
-    public LinkedHashMap dict;
 
     public static boolean createUser(String firstName,
                               String lastName,
                               String email,
-                              String pwd,
-                              LinkedHashMap dict)
+                              String pwd)
     {
-       //????? dict = <firstName,lastName>;
+        //Creates new JSONObjects to pass to and receive from Requester.requester
+        //  Note: responses need to be in Atomic Reference since the requester works on a different thread.
+        JSONObject requestGET = new JSONObject();
+        AtomicReference<JSONObject> responseGET = new AtomicReference<>(new JSONObject());
+        JSONObject requestPOST = new JSONObject();
+        AtomicReference<JSONObject> responsePOST = new AtomicReference<>(new JSONObject());
+        //????? dict = <firstName,lastName>;
 
         //Request info from requester to see if already exists
-        Requester.requester("/user", "GET", dict);
+        responseGET.set(Requester.requester("/user", "GET", requestGET));
             //if it returns something, then it already exists ???
 
         //If it already exists
@@ -36,11 +43,33 @@ public class User {
         //If it doesn't exist yet
         else
         {
+
+            try {
+                //Populates the object with keys and values
+                //  e.g. "first_name": "Jim"
+                //  Note: keys need to be in python format since the api is written in python (e.g. first_name)
+                //        while values can be in java format (e.g. firstName)
+                requestPOST.put("first_name", firstName);
+                requestPOST.put("last_name", lastName);
+                requestPOST.put("email", email);
+                requestPOST.put("password", pwd);
+            } catch (JSONException e){ //Catch necessary since dictToApi.put can throw the exception JSONException
+                //Prints the error message to the console via stacktrace
+                e.printStackTrace();
+            }
             //Create it here
             //  ...
             //maybe this
-               Requester.requester("/user", "POST",
-                       dict);
+            //Saves the output of Request.requester to a JSONObject responsePOST
+            responsePOST.set(Requester.requester("/user", "POST", requestPOST));
+
+            try {
+                //Searches for "user_id" as a key in the responsePOST.
+                Object userID = responsePOST.get().get("user_id");
+            } catch (JSONException e){ //Catch necessary since responsePOST.get can throw the exception JSONException
+            //Prints the error message to the console via stacktrace
+            e.printStackTrace();
+            }
 
             return true;
         }
