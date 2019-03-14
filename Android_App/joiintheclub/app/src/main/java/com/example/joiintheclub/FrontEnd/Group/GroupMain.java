c6e1718c15e17130f1785e489e805eef9a783ad6
JIAHUI J <jiahui.jiang@biola.edu>
2019-03-14 10:54:05 -0700

    Creation of created account, search, profile, group, setting activities

---
+++ Android_App/joiintheclub/app/src/main/java/com/example/joiintheclub/FrontEnd/Group/GroupMain.java
@@ -0,0 +1,16 @@
+package com.example.joiintheclub.FrontEnd.Group;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.joiintheclub.R;
+
+public class GroupMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_main);
+    }
+
+}
